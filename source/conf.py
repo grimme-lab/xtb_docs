0f9ee127bed6f024c6edf24395885ce8f4d37d47
Eike Caldeweyher <46899008+f3rmion@users.noreply.github.com>
2019-04-15 12:28:09 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Eike Caldeweyher'
 author = 'Eike Caldeweyher'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '6.1'
 
 
 # -- General configuration ---------------------------------------------------
