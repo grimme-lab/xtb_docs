2b02bfbdf4973e29762355db0ea0a13588fff0f9
fabothch <49951809+fabothch@users.noreply.github.com>
2019-04-26 15:30:21 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
- import os
- import sys
- sys.path.insert(0, os.path.abspath('.'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -30,7 +30,7 @@ release = '6.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['hidden_code_block'
+extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
