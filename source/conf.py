8a4ed8bea52ab6b8541c3534b77ad6a070b32c62
fabothch <49951809+fabothch@users.noreply.github.com>
2019-05-07 14:13:41 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -30,8 +30,8 @@ release = '6.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinxcontrib.contentui'
+             ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
