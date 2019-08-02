e99602a3c97a09cccdd43f98d99bd50b41cff71c
awvwgk <28669218+awvwgk@users.noreply.github.com>
2019-08-02 11:02:05 +0200

    updated version number

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Grimme group'
 author = 'Grimme group'
 
 # The full version, including alpha/beta/rc tags
-release = '6.1'
+release = '6.2'
 
 
 # -- General configuration ---------------------------------------------------
