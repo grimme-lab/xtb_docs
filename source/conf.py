e74de4d371c5ff0df8b532a9a439158cd15e4529
fabothch <49951809+fabothch@users.noreply.github.com>
2021-03-15 15:48:22 +0100

    wide tables

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,8 @@
 #import sys
 #sys.path.insert(0, os.path.abspath('.'))
 
-
+def setup(app):
+    app.add_css_file('custom.css')
 # -- Project information -----------------------------------------------------
 
 project = 'xtb doc'
@@ -62,7 +63,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
 
 # other default
 master_doc = 'contents'
