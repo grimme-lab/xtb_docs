2559fc97d04b55c0cc85c3b56e9f2c320d38762d
Fabian Bohle <49951809+fabothch@users.noreply.github.com>
2021-03-15 16:17:21 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,8 +14,7 @@
 #import sys
 #sys.path.insert(0, os.path.abspath('.'))
 
-def setup(app):
-    app.add_css_file('custom.css')
+
 # -- Project information -----------------------------------------------------
 
 project = 'xtb doc'
@@ -63,6 +62,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+html_css_files = [ "custom.css", ]
 html_static_path = ['_static']
 
 # other default
